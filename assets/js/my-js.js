74b10c059fddd4930e94fb7fdd46cc9a5d0c457a
ranjidalii <ranjid.alii@gmail.com>
2025-04-25 10:59:05 +0700

    update 2025 | foto project

--- assets/js/my-js.js
+++ assets/js/my-js.js
@@ -11,6 +11,12 @@ const itemDetailButton3 = document.querySelector(".item-detail-button-3");
 const itemDetailModal4 = document.querySelector("#item-detail-modal-4");
 const itemDetailButton4 = document.querySelector(".item-detail-button-4");
 
+const itemDetailModal5 = document.querySelector("#item-detail-modal-5");
+const itemDetailButton5 = document.querySelector(".item-detail-button-5");
+
+const itemDetailModal6 = document.querySelector("#item-detail-modal-6");
+const itemDetailButton6 = document.querySelector(".item-detail-button-6");
+
 itemDetailButton1.onclick = (e) => {
   itemDetailModal1.style.display = "flex";
   e.preventDefault();
@@ -28,6 +34,16 @@ itemDetailButton3.onclick = (e) => {
 
 itemDetailButton4.onclick = (e) => {
   itemDetailModal4.style.display = "flex";
+  e.preventDefault();
+};
+
+itemDetailButton5.onclick = (e) => {
+  itemDetailModal5.style.display = "flex";
+  e.preventDefault();
+};
+
+itemDetailButton6.onclick = (e) => {
+  itemDetailModal6.style.display = "flex";
   e.preventDefault();
 };
 
@@ -52,21 +68,37 @@ document.querySelector(".my_modal_4 .close-icon").onclick = (e) => {
   e.preventDefault();
 };
 
+document.querySelector(".my_modal_5 .close-icon").onclick = (e) => {
+  itemDetailModal5.style.display = "none";
+  e.preventDefault();
+};
+
+document.querySelector(".my_modal_6 .close-icon").onclick = (e) => {
+  itemDetailModal6.style.display = "none";
+  e.preventDefault();
+};
+
 // Klik di luar Modal
 const my_modal_1 = document.querySelector("#item-detail-modal-1");
 const my_modal_2 = document.querySelector("#item-detail-modal-2");
 const my_modal_3 = document.querySelector("#item-detail-modal-3");
 const my_modal_4 = document.querySelector("#item-detail-modal-4");
+const my_modal_5 = document.querySelector("#item-detail-modal-5");
+const my_modal_6 = document.querySelector("#item-detail-modal-6");
 window.onclick = (e) => {
   if (
     e.target === itemDetailModal1 ||
     e.target === itemDetailModal2 ||
     e.target === itemDetailModal3 ||
-    e.target === itemDetailModal4
+    e.target === itemDetailModal4 ||
+    e.target === itemDetailModal5 ||
+    e.target === itemDetailModal6
   ) {
     my_modal_1.style.display = "none";
     my_modal_2.style.display = "none";
     my_modal_3.style.display = "none";
     my_modal_4.style.display = "none";
+    my_modal_5.style.display = "none";
+    my_modal_6.style.display = "none";
   }
 };
